b7217282999cd416776bbfe441e74b084cbee139
Rebeca <rebecadmsilva@gmail.com>
2026-07-18 18:24:28 -0300

    refactor: rename InterfaceEstrutura to PriorityQueue

--- src/main/java/model/PriorityQueue.java
+++ src/main/java/model/PriorityQueue.java
@@ -1,6 +1,6 @@
 package model;
 
-public interface InterfaceEstrutura {
+public interface PriorityQueue {
     public void add(int value);
     public int remove(int key); // heap sempre remove o 0
     public int search(int key);
