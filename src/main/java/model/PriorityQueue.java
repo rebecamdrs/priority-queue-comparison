c30563e391aa86f18edde9d9f26ea4f67893acdb
Rebeca <rebecadmsilva@gmail.com>
2026-07-19 10:58:18 -0300

    refactor: change remove() to always extract the minimum element

--- src/main/java/model/PriorityQueue.java
+++ src/main/java/model/PriorityQueue.java
@@ -2,6 +2,6 @@ package model;
 
 public interface PriorityQueue {
     public void add(int value);
-    public int remove(int key); // heap sempre remove o 0
-    public int search(int key);
+    public int removeMin();
+    public int search(int value);
 }
